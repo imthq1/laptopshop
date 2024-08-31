6445c92d3730a9b41125834233315b963c289dc0
Nguyen Toan Thang <xamthang09@gmail.com>
2024-08-31 10:55:02 +0700

    validate Login, Register

---
+++ src/main/java/com/example/demo/service/validator/RegisterValidator.java
@@ -0,0 +1,45 @@
+package com.example.demo.service.validator;
+
+import com.example.demo.domain.DTO.RegisterDTO;
+import com.example.demo.service.UserService;
+import com.example.demo.service.validator.RegisterChecked;
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+
+@Service
+public class RegisterValidator implements ConstraintValidator<RegisterChecked, RegisterDTO> {
+    private final UserService userService;
+
+    public RegisterValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean isValid(RegisterDTO user, ConstraintValidatorContext context) {
+        boolean valid = true;
+
+        // Check if password fields match
+        if (!user.getPassword().equals(user.getConfirmPassword())) {
+            context.buildConstraintViolationWithTemplate("Passwords nhập không chính xác")
+                    .addPropertyNode("confirmPassword")
+                    .addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+            valid = false;
+        }
+
+        // Additional validations can be added here
+        // check email
+        if (this.userService.checkEmailExists(user.getEmail())) {
+            context.buildConstraintViolationWithTemplate("Email đã tồn tại")
+                    .addPropertyNode("email")
+                    .addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+            valid = false;
+        }
+
+        return valid;
+    }
+}
